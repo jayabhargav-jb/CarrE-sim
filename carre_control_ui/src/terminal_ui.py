4baad9cd4925707cceea63a6773ef600fcedf13d
Jaya Bhargav P <jayabhargav.jb@gmail.com>
2024-05-08 09:29:12 +0530

    modified terminal_ui.py and model files

--- carre_control_ui/src/terminal_ui.py
+++ carre_control_ui/src/terminal_ui.py
@@ -19,23 +19,27 @@ Moving around:
    j    k    l
    m    ,    .
 
-For Holonomic mode (strafing), hold down the shift key:
----------------------------
-   U    I    O
-   J    K    L
-   M    <    >
-
-t : up (+z)
-b : down (-z)
-
-anything else : stop
-
-q/z : increase/decrease max speeds by 10%
-w/x : increase/decrease only linear speed by 10%
-e/c : increase/decrease only angular speed by 10%
-
 CTRL-C to quit
 """
+
+# """
+# For Holonomic mode (strafing), hold down the shift key:
+# ---------------------------
+#    U    I    O
+#    J    K    L
+#    M    <    >
+
+# t : up (+z)
+# b : down (-z)
+
+# anything else : stop
+
+# q/z : increase/decrease max speeds by 10%
+# w/x : increase/decrease only linear speed by 10%
+# e/c : increase/decrease only angular speed by 10%
+
+# """
+
 
 moveBindings = {
         'i':(1,0,0,0),
