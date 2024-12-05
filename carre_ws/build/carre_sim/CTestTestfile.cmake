6d6221d28d6ac95b819ab8cd584125a047460463
Jaya Bhargav P <jayabhargav.jb@gmail.com>
2024-12-05 12:36:06 +0530

    removed build files

--- carre_ws/build/carre_sim/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /carre_ws/src/carre_sim
-# Build directory: /carre_ws/build/carre_sim
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
